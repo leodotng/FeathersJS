1343f9b5716806bff768fea3109c036b28b3e44d
Josh Leong <josh@leo.ng>
2019-08-28 00:29:31 -0600

    created app2.js to find diff problem

--- app2.js
+++ app2.js
@@ -5,28 +5,28 @@ const socketio = require('@feathersjs/socketio');
 // A messages service that allows to create new
 // and return all existing messages
 class MessageService {
-    constructor() {
-        this.messages = [];
+  constructor() {
+    this.messages = [];
+  }
+
+  async find () {
+    // Just return all our messages
+    return this.messages;
+  }
+
+  async create (data) {
+    // The new message is the data merged with a unique identifier
+    // using the messages length since it changes whenever we add one
+    const message = {
+      id: this.messages.length,
+      text: data.text
     }
 
-    async find () {
-        // Just return all our messages
-        return this.messages;
-    }
+    // Add new message to the list
+    this.messages.push(message);
 
-    async create (data) {
-        // The new message is the data merged with unique identifier
-        // using the messages length since it changes whenever we add one
-        const message = {
-            id: this.messages.length,
-            text: data.text
-        }
-
-        // Add new message to the list
-        this.messages.push(message);
-
-        return message;
-    }
+    return message;
+  }
 }
 
 // Creates an ExpressJS compatible Feathers application
@@ -48,19 +48,19 @@ app.use('/messages', new MessageService());
 app.use(express.errorHandler());
 
 // Add any new real-time connection to the `everybody` channel
-app.on('connection', connection => 
-    app.channel('everybody').join(connection)
+app.on('connection', connection =>
+  app.channel('everybody').join(connection)
 );
 // Publish all events to the `everybody` channel
 app.publish(data => app.channel('everybody'));
 
 // Start the server
 app.listen(3030).on('listening', () =>
-    console.log('Feathers server listening on localhost:3030')
+  console.log('Feathers server listening on localhost:3030')
 );
 
 // For good measure let's create a message
 // So our API doesn't look so empty
 app.service('messages').create({
-    text: 'Hello world from the server'
+  text: 'Hello world from the server'
 });
